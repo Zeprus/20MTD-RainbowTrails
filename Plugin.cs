b43a4ba09a1b28d712d3eefd78977f8c7186a33c
Zeprus <zeprusaliastrylon@googlemail.com>
2022-06-16 15:28:56 +0200

    RainbowTrails initial commit

---
+++ Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+using HarmonyLib;
+
+namespace RainbowTrails
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class RainbowTrails : BaseUnityPlugin
+    {
+        internal static ManualLogSource Log;
+
+        public RainbowTrails()
+        {
+            Log = base.Logger;
+            try
+            {
+                Harmony.CreateAndPatchAll(typeof(Patch.ProjectileFactory_Patch));
+            }
+            catch
+            {
+                Log.LogWarning("Harmony failed to patch RainbowTrails");
+            }
+        }
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+        }
+    }
+}
---
+++ ProjectileFactory.cs
@@ -0,0 +1,50 @@
+using HarmonyLib;
+
+using flanne;
+using UnityEngine;
+
+namespace RainbowTrails.Patch
+{
+    class ProjectileFactory_Patch
+    {
+        static Color startColor = new Color(148, 0, 211);
+        static Color endColor = new Color(255, 0, 0);
+        static GradientColorKey[] colorKeys = new GradientColorKey[7] {
+            new GradientColorKey(new Color(148, 0, 211), 0.0f),
+            new GradientColorKey(new Color(75, 0, 130), 1/6f),
+            new GradientColorKey(new Color(0, 0, 255), 2/6f),
+            new GradientColorKey(new Color(0, 255, 0), 3/6f),
+            new GradientColorKey(new Color(255, 255, 0), 4/6f),
+            new GradientColorKey(new Color(255, 127, 0), 5/6f),
+            new GradientColorKey(new Color(255, 0 , 0), 1.0f),
+        };
+        static GradientAlphaKey[] alphaKeys = new GradientAlphaKey[7]{
+            new GradientAlphaKey(1.0f, 0f),
+            new GradientAlphaKey(1.0f, 1/6f),
+            new GradientAlphaKey(1.0f, 2/6f),
+            new GradientAlphaKey(1.0f, 3/6f),
+            new GradientAlphaKey(1.0f, 4/6f),
+            new GradientAlphaKey(1.0f, 5/6f),
+            new GradientAlphaKey(1.0f, 1f),
+        };
+
+
+
+        [HarmonyPatch(typeof(ProjectileFactory), "SpawnProjectile")]
+        [HarmonyPostfix]
+        static void ProjectileTrailPostfix(ref Projectile __result)
+        {
+            //get protected property Projectile TrailRenderer with no getter
+            TrailRenderer trailRenderer = (TrailRenderer)Traverse.Create(__result).Field("trail").GetValue();
+
+            trailRenderer.startColor = ProjectileFactory_Patch.startColor;
+            trailRenderer.endColor = ProjectileFactory_Patch.endColor;
+            //this is a BAD idea
+            //trailRenderer.time = trailRenderer.time * 5;
+            Gradient gradient = new Gradient();
+            gradient.SetKeys(ProjectileFactory_Patch.colorKeys, ProjectileFactory_Patch.alphaKeys);
+            gradient.mode = GradientMode.Fixed;
+            trailRenderer.SetColorGradient(gradient);
+        }
+    }
+}
